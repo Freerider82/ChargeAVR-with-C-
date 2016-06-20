048e2fa61dddd73f1f0200e2ed4050a55cd5b846
Oleg Kolodchenko <sfet1507@gmail.com>
2016-06-20 18:46:14 +0300

    Begin

---
+++ ChargeAVR/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * ChargeAVR.cpp
+ *
+ * Created: 20.06.2016 18:37:18
+ * Author : OlegAvr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
