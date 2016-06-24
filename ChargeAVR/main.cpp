e10850c9563853661fbc7d04d7c84c7eff12e552
Oleg Kolodchenko <sfet1507@gmail.com>
2016-06-24 19:04:51 +0300

    Добавили клас Timer сделали код кнопки 5сек

--- ChargeAVR/main.cpp
+++ ChargeAVR/main.cpp
@@ -10,53 +10,68 @@
 #include <avr/io.h>
 #include "buttons/buttons.h"
 #include "ioport/IOPort.h"
+#include "timers/timer.h"
 #include <stdbool.h>
 #include <util/delay.h>
+#include "../../../../../toolchain/avr8/avr8-gnu-toolchain/avr/include/avr/interrupt.h"
 
 
-
+#define TIME_1_SEC				4
+#define TIMER0_INTERRUPT		244
+void TIMER0_Init();
 
 IOPort portA(&PORTA, &PINA, &DDRA);
 IOPort portB(&PORTB, &PINB, &DDRB);
 IOPort portC(&PORTC, &PINC, &DDRC);
 
-int main(void)
-{
+Timer time;
+volatile uint8_t time1000ms=TIME_1_SEC;
+int main(void){
+	
 	portA.SetPortDirection(0xFF);
 	portB.SetPortDirection(0xFF);
 	portC.SetPortDirection(0xFF);
+		
+	TIMER0_Init();
 	
-	Button button(&PIND, &DDRD,3,PD1,PD2,PD3);
+	Button button(&PIND, &DDRD,&time,3,PD1,PD2,PD3);
 	
 	
     /* Replace with your application code */
-	
+	sei();
 	while(1){
 		
 		//portB.SetPortValues(button.getCurrentStatus());
 		//portC.SetPortValues(button.getPreviousStatus());
 		if(button.buttonWasPressed()){
-			portB.SetPortValues(button.getCurrentStatus());
-			portA.SetPortValues(button.getCodePressedButton());
-		} else{
 			
-		}
+			portA.SetPortValues(button.getCodePressedButton());
+		} 
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-    while (1) 
-    {
-		
-		
-    }
 }
-
+/****************************************************************************/
+	
+void TIMER0_Init(){
+	//Коэф деления 1024 это примерно 1 мс при 1 МГц
+	TCCR0|=(1<<CS02)|(1<<CS00);
+	TCNT0=0;
+	TIMSK|=(1<<OCIE0);
+	OCR0=TIMER0_INTERRUPT;
+}	
+/****************************************************************************/
+	
+ISR(TIMER0_COMP_vect){
+	time1000ms--;	
+	if(time1000ms==0){
+		time1000ms=TIME_1_SEC;
+		time.incrTime();
+				
+	}
+}	
+	
+	
+	
+	
+	
+	
 
